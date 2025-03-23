8d7e1ba123df34f61b17b7ad7851828659a2cb94
SumanK307 <sumankumarcse2026@gmail.com>
2025-03-23 22:53:59 +0530

    Add new

--- script.js
+++ script.js
@@ -9,4 +9,7 @@ const navlink = document.querySelectorAll('header nav a')
 window.onscroll = ()=>{
     section.forEach(sec=>{
         let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
         
