f754470153b07e104ac81190137eeab6738e65ba
SumanK307 <sumankumarcse2026@gmail.com>
2025-05-10 16:27:57 +0530

    Add new

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const navlink = document.querySelectorAll('header nav a')
 window.onscroll = () => {
     section.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id')
         if (top > offset && top < offset + height) {
@@ -21,7 +21,7 @@ window.onscroll = () => {
         }
     })
     var header = document.querySelector('.header');
-    header.classList.toggle('sticky', window.scrollY >30)
+    header.classList.toggle('sticky', window.scrollY > 30)
     menubar.classList.remove('bx-x');
     Navbar.classList.remove('active')
 }
